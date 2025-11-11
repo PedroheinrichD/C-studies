4864585b63e97f3e30b306201b0bc924c1fb6c19
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-11-11 18:20:27 -0300

    Adiciona programa para mapear número ao dia da semana

--- main.c
+++ main.c
@@ -3,18 +3,64 @@
 
 // o algoritmo deve informar se o numero é par ou impar 
 int main() {
-    int m;
-    printf("Digite á média");
-    scanf("%d",&m);
+    int n;
+
+    printf("digite o numero da semana \n");
+    scanf("%d",&n);
+
+    switch (n)
+    {
+    case 1 : printf("DOMINGO\n");
+        break;
+    case 2 : printf("SEGUNDA \n");
+        break;
+    case 3 : printf("TERÇA \n");
+        break;
+    case 4 : printf("QUARTA \n");
+        break;
+    case 5 : printf("QUINTA \n");
+        break;
+    case 6 : printf("SEXTA \n");
+        break;
+    case 7 : printf("SÁBADO \n");
+        break;
     
-    if(m > 7){
-        printf("APROVADO COM A MÉDIA: %d ",m);
-    }else if(m >= 4){
-        printf("FAZER PROVA SUBSTITUTIVA MÉDIA: %d ",m);
-    }else{
-        printf("REPROVADO COM A MÉDIA: %d ",m);
+    default: printf("Digite um número de 1 a 7 para ver o dia da semana correspondente.\n");
+        break;
     }
-    
-    
+
     return 0;
 }
+
+
+// Quando o usuário digita aparece um dia da semana
+/*int main() {
+    int n;
+
+    printf("digite o numero da semana \n");
+    scanf("%d",&n);
+
+    switch (n)
+    {
+    case 1 : printf("DOMINGO\n");
+        break;
+    case 2 : printf("SEGUNDA \n");
+        break;
+    case 3 : printf("TERÇA \n");
+        break;
+    case 4 : printf("QUARTA \n");
+        break;
+    case 5 : printf("QUINTA \n");
+        break;
+    case 6 : printf("SEXTA \n");
+        break;
+    case 7 : printf("SÁBADO \n");
+        break;
+    
+    default: printf("Digite um número de 1 a 7 para ver o dia da semana correspondente.\n");
+        break;
+    }
+
+    return 0;
+}
+*/
